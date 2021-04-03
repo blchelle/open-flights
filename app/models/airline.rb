36290d332a08d43272a3990439135b1aab0cca1a
Brock Chelle <Brocklchelle@gmail.com>
2021-04-02 18:42:50 -0600

    Initialized the Airline model

---
+++ app/models/airline.rb
@@ -0,0 +1,14 @@
+class Airline < ApplicationRecord
+  has_many :reviews
+
+  before_create :slugify
+
+  def slugify
+    self.slug = name.parameterize
+  end
+
+  def avg_score
+    # Rounds the average review score to 2 digits
+    reviews.average(:score).round(2).to_f
+  end
+end
