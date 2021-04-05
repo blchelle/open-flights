718c350f07c90f19680c72815bc063e8efd268d5
Brock Chelle <Brocklchelle@gmail.com>
2021-04-05 09:06:39 -0600

    Setup typescript for client

---
+++ app/javascript/components/AirlineItem/AirlineItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AirlineItem = () => {
+  return <div></div>;
+};
+
+export default AirlineItem;
---
+++ app/javascript/pages/Airline/Airline.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Airline = () => {
+  return <div>This is the airlines show view</div>;
+};
+
+export default Airline;
