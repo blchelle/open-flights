4aa2501de4275186cb1d2679545a1529473feb14
Brock Chelle <Brocklchelle@gmail.com>
2021-04-07 10:30:29 -0600

    Added avg_score to the Airline Serializer

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -9,6 +9,8 @@ class Airline < ApplicationRecord
   end
 
   def avg_score
+    return 0 unless reviews.count.positive?
+
     # Rounds the average review score to 2 digits
     reviews.average(:score).round(2).to_f
   end
